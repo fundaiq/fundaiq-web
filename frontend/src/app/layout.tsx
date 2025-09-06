9e232869a52519fe396c40c353a0fc08f7d55433
PaiseDePange <PaiseDePange@gmail.com>
2025-09-06 18:53:41 +0100

    analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -89,6 +89,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <ScrollToTopButton />
           <GlobalScripts />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
